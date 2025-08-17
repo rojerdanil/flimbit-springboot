3c7d3b6a3d5683011e7c149f795091fb365e6c03
rojer <rojer127@gmail.com>
2025-08-17 08:54:59 +0530

    files chagnes cors added

--- src/main/java/com/riseup/flimbit/configuration/WebConfig.java
+++ src/main/java/com/riseup/flimbit/configuration/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // allow all endpoints
-                .allowedOrigins("http://localhost", "http://localhost:80") // allow all origins (not recommended in production)
+                .allowedOrigins("http://localhost", "http://localhost:80","https://filmbit-home.onrender.com") // allow all origins (not recommended in production)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "accesstoken", "deviceid", "phonenumber","x-device-id","x-device-type") // Explicitly list headers
                 .allowCredentials(true);
